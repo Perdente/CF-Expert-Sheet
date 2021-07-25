b799205951d4305eeaaf2a234d2f91beaf5d03af
Loser <58706003+Perdente@users.noreply.github.com>
2021-07-25 23:53:14 +0600

    Create a-Good String(req).cpp

---
+++ specialist-quest/a-Good String(req).cpp
@@ -0,0 +1,43 @@
+//https://codeforces.com/contest/1385/problem/D
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+void Malena(){
+	int n;cin>>n;
+	char arr[n];
+	for(int i=0;i<n;++i){
+		cin>>arr[i];
+	}
+	auto cost=[&](int l,int r,char ch)->int{
+		int cnt=0;
+		for(int i=l;i<=r;++i)
+			cnt+=(arr[i]!=ch);
+		return cnt;
+	};
+	function<int(int,int,char)>getmin=[&](int l,int r,char ch)->int{
+		if(l==r){
+			if(arr[l]==ch)return 0;
+			else return 1;
+		}
+		int m=(l+r)/2;
+		return min( (cost(l,m,ch) + getmin(m+1,r,ch+1)) , (getmin(l,m,ch+1) + cost(m+1,r,ch)) );
+	};
+	cout<<getmin(0,n-1,'a')<<'\n';
+}
+
+signed main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t;
+	t=1;
+	cin>>t;
+	while(t--){
+		Malena();
+	}
+	return 0;
+}
